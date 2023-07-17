977cd754ecebf18575e1a0005b9b63b9be646fc1
TJ Li <TianjianLi314@gmail.com>
2023-07-16 21:05:33 -0500

    Added new route that adds new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages }, );
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form')
+});
+
+router.post('/new', (req, res, next) => {
+  user = req.body.username;
+  text = req.body.description;
+  messages.push({text: text, user: user, added: new Date()});
+  res.redirect('/');
+});
+
 
 module.exports = router;
